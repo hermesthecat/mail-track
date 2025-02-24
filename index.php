81bfde9870e66fc77f58f437e55335c2c28557c3
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-02-24 11:17:16 +0300

    Remove table-hover class from campaigns table
    - Simplify table styling by removing redundant hover class
    - Maintain clean and consistent table presentation

--- index.php
+++ index.php
@@ -422,7 +422,7 @@ if (isset($_GET['api'])) {
                                 </button>
                             </div>
                             <div class="table-responsive">
-                                <table class="table table-hover campaigns-table" id="campaignsTable">
+                                <table class="table table-hover" id="campaignsTable">
                                     <thead>
                                         <tr>
                                             <th>Kampanya Adı</th>
